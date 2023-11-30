514ba5587756525c14ebd87de9007797b1f65574
Nico Hauser <me@tyratox.ch>
2023-11-30 08:32:02 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.DirectoryServices.Protocols;
+
+var useTls = true;
+var useJumpcloud = true;
+
+var port = useTls ? 636 : 389;
+
+Console.WriteLine("setup connection");
+
+LdapConnection connection;
+
+if (useJumpcloud)
+{
+    // does not work with TLS enabled, crashes badly
+    connection = new LdapConnection($"ldap.jumpcloud.com:{port}")
+    {
+        AuthType = AuthType.Basic
+    };
+}
+else
+{
+    // works
+    connection = new LdapConnection($"ipa.demo1.freeipa.org:{port}")
+    {
+        AuthType = AuthType.Basic
+    };
+}
+
+
+
+Console.WriteLine("setup options");
+
+connection.SessionOptions.VerifyServerCertificate = (_, _) => true;
+connection.SessionOptions.ProtocolVersion = 3;
+
+if (useTls)
+{
+    connection.SessionOptions.SecureSocketLayer = true;
+}
+
+Console.WriteLine("bind");
+
+connection.Bind(/*credentials*/);
+
+Console.WriteLine("yay");
